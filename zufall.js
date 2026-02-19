da2202603a3b7ab59f3237cf8bf4b77ae9480193
gentian-ai <gentian.muqiqi9@gmail.com>
2026-02-19 13:41:34 +0100

    Tag 2

--- zufall.js
+++ zufall.js
@@ -1,4 +1,5 @@
 const random = Math.floor(Math.random() * 11);
+let versuche = 0;
 // console.log(random)
 import { createInterface } from 'node:readline';
 
@@ -7,16 +8,35 @@ const rl = createInterface({
     output: process.stdout
 });
 
-rl.question('Nenne mir eine Zahl zwischen 0 bis 10: ', (zahl) => {
+function spiel (text) {
+rl.question(text, (zahl) => {
     if (Number(zahl) === random) {
         console.log(`${zahl} ist richtig!`);
-    }
-    else {
-        console.log(`${zahl} ist falsch. Es ist ${random}`);
-    }
     rl.close();
+    }
+    else if (Number(zahl) > random && versuche<3) {
+        versuche++;
+        if (versuche === 3){
+            console.log(`Du hast zu viele Versuche gebraucht. Es war ${random}. Verloren!!!`);
+            rl.close();
+        }
+        else {
+            spiel(`${zahl} ist zu hoch. Du hast schon ${versuche} von 3 Versuchen genutzt. Rate niedriger!`);
+        }
+    }
+    else if (Number(zahl) < random && versuche<3) {
+        versuche++;
+        if (versuche === 3){
+            console.log(`Du hast zu viele Versuche gebraucht. Es war ${random}. Verloren!!!`);
+            rl.close();
+        }
+        else {
+            spiel(`${zahl} ist zu tief. Du hast schon ${versuche} von 3 Versuchen genutzt. Rate höher!`);
+        }
+    }
+    else if (isNaN(zahl)) {
+        spiel(`${zahl} ist doch keine Zahl? Gib bitte eine gültige Zahl an!`);
+    }
 });
-
-if (true == true) {
-
 }
+spiel("Nenne mir eine Zahl zwischen 0 bis 10: ");
