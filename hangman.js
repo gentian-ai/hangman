ff02f6f5661669a1d7dd833b32a6e2ff8e52edc2
gentian-ai <gentian.muqiqi9@gmail.com>
2026-02-20 16:18:37 +0100

    Tag 3

--- hangman.js
+++ hangman.js
@@ -1,19 +1,70 @@
-const woerter = ["Apfel", "Baum", "Computer", "Dach", "Elefant", "Fahrrad", "Gitarre", "Haus", "Insel", "Jacke",
-    "Kaffee", "Lampe", "Maus", "Nase", "Ozean", "Papier", "Qualle", "Radio", "Sonne", "Tisch",
-    "Uhr", "Vogel", "Wald", "Xylophon", "Yacht", "Zebra", "Ameise", "Besen", "Clown", "Drache",
-    "Eis", "Frosch", "Garten", "Honig", "Igel", "Junge", "Keks", "Loewe", "Mond", "Nacht",
-    "Onkel", "Park", "Qualitaet", "Reis", "Schuh", "Tiger", "Uhu", "Vase", "Wolke", "Zug",
-    "Anker", "Brot", "Couch", "Daumen", "Eimer", "Finger", "Gabel", "Hammer", "Idol", "Joghurt",
-    "Krone", "Lippe", "Messer", "Nadel", "Oper", "Pinsel", "Quark", "Ritter", "Sessel", "Tasse",
-    "Ufer", "Vulkan", "Wiese", "Zange", "Abend", "Blitz", "Chaos", "Delfin", "Erde", "Feuer",
-    "Geist", "Himmel", "Imbiss", "Juwel", "Krieger", "Licht", "Motor", "Nuss", "Opa", "Pfeil",
-    "Quelle", "Rauch", "Stern", "Traum", "Urlaub", "Ventil", "Wueste", "Zwerg", "Axt", "Boot"]; 
+import { createInterface } from 'node:readline';
 
-const random = Math.floor(Math.random() * woerter.length);
-let versuche = 0;
-let wort = woerter[random];
-const maske = "_ ".repeat(wort.length);
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(maske);
-console.log(wort);
-console.log(random)
+const words = ["apfel", "baum", "computer", "dach", "elefant", "fahrrad", "gitarre", "haus", "insel", "jacke",
+    "kaffee", "lampe", "maus", "nase", "ozean", "papier", "qualle", "radio", "sonne", "tisch",
+    "uhr", "vogel", "wald", "xylophon", "yacht", "zebra", "ameise", "besen", "clown", "drache",
+    "eis", "frosch", "garten", "honig", "igel", "junge", "keks", "loewe", "mond", "nacht",
+    "onkel", "park", "qualitaet", "reis", "schuh", "tiger", "uhu", "vase", "wolke", "zug",
+    "anker", "brot", "couch", "daumen", "eimer", "finger", "gabel", "hammer", "idol", "joghurt",
+    "krone", "lippe", "messer", "nadel", "oper", "pinsel", "quark", "ritter", "sessel", "tasse",
+    "ufer", "vulkan", "wiese", "zange", "abend", "blitz", "chaos", "delfin", "erde", "feuer",
+    "geist", "himmel", "imbiss", "juwel", "krieger", "licht", "motor", "nuss", "opa", "pfeil",
+    "quelle", "rauch", "stern", "traum", "urlaub", "ventil", "wueste", "zwerg", "axt", "boot"];
+
+const random = Math.floor(Math.random() * words.length);
+let mistakes = 0;
+let word = words[random];
+let guess = [];
+const mask = "_ ".repeat(word.length);
+
+
+
+function showWord() {
+    let show = "";
+    for (let buchstabe of word) {
+        if (guess.includes(buchstabe)) {
+            show += buchstabe + " ";
+        } else {
+            show += "_ ";
+        }
+    }
+    return show;
+}
+
+
+function spiel (text) {
+    rl.question(text, (buchstabe) => {
+        guess.push(buchstabe);
+        const aktuelleAnzeige = showWord();
+        console.log(aktuelleAnzeige);
+
+        if (!showWord().includes("_")) {
+            console.log("GLÜCKWUNSCH! Du hast das Wort erraten!");
+            rl.close();
+            return;
+        }
+        if (word.includes(buchstabe)) {
+            spiel("Richtig! Der Buchstabe ist dabei! Weiter geht es!");
+        }
+        else {
+            mistakes++;
+        }
+        if (mistakes === 10) {
+            console.log(`Du hast leider keine Versuche mehr. Es war ${word}. Viel Erfolg beim nächsten mal!`)
+            rl.close()
+        }
+        else {
+            spiel(`Falsch. Der Buchstabe ist nicht dabei. Du kannst nur noch ${10 - mistakes} Fehler machen.`)
+        }
+    })
+}
+
+    //console.log(word)
+
+console.log(mask)
+spiel("Welchen Buchstaben rätst du?")
--- zufall.js
+++ zufall.js
@@ -1,42 +1,44 @@
-const random = Math.floor(Math.random() * 11);
-let versuche = 0;
-// console.log(random)
 import { createInterface } from 'node:readline';
+const random = Math.floor(Math.random() * 11);
+let trys = 0;
+const max = 3
+
+// console.log(random)
 
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function spiel (text) {
-rl.question(text, (zahl) => {
-    if (Number(zahl) === random) {
-        console.log(`${zahl} ist richtig!`);
-    rl.close();
-    }
-    else if (Number(zahl) > random && versuche<3) {
-        versuche++;
-        if (versuche === 3){
-            console.log(`Du hast zu viele Versuche gebraucht. Es war ${random}. Verloren!!!`);
+function spiel(text) {
+    rl.question(text, (zahl) => {
+        //rl.close();
+        if (Number(zahl) === random) {
+            console.log(`${zahl} ist richtig!`);
             rl.close();
         }
-        else {
-            spiel(`${zahl} ist zu hoch. Du hast schon ${versuche} von 3 Versuchen genutzt. Rate niedriger!`);
+        else if (Number(zahl) > random && trys < max) {
+            trys++;
+            if (trys === 3) {
+                console.log(`Du hast zu viele Versuche gebraucht. Es war ${random}. Verloren!!!`);
+            }
+            else {
+                spiel(`${zahl} ist zu hoch. Du hast schon ${trys} von 3 Versuchen genutzt. Rate niedriger!`);
+            }
         }
-    }
-    else if (Number(zahl) < random && versuche<3) {
-        versuche++;
-        if (versuche === 3){
-            console.log(`Du hast zu viele Versuche gebraucht. Es war ${random}. Verloren!!!`);
-            rl.close();
+        else if (Number(zahl) < random && trys < max) {
+            trys++;
+            if (trys === 3) {
+                console.log(`Du hast zu viele Versuche gebraucht. Es war ${random}. Verloren!!!`);
+            }
+            else {
+                spiel(`${zahl} ist zu tief. Du hast schon ${trys} von 3 Versuchen genutzt. Rate höher!`);
+            }
         }
-        else {
-            spiel(`${zahl} ist zu tief. Du hast schon ${versuche} von 3 Versuchen genutzt. Rate höher!`);
+        else if (isNaN(zahl)) {
+            spiel(`${zahl} ist doch keine Zahl? Gib bitte eine gültige Zahl an!`);
         }
-    }
-    else if (isNaN(zahl)) {
-        spiel(`${zahl} ist doch keine Zahl? Gib bitte eine gültige Zahl an!`);
-    }
-});
+    });
 }
+
 spiel("Nenne mir eine Zahl zwischen 0 bis 10: ");
